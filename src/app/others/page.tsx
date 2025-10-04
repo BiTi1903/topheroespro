ef02c71f6c3e0744b6e4b78bbcf8e87487c31b0b
BiTI <lethien1932003@gmail.com>
2025-10-04 09:54:37 +0700

    ok

--- src/app/others/page.tsx
+++ src/app/others/page.tsx
@@ -3,12 +3,20 @@ export default function OthersPage() {
     <div className="p-6">
       <h1 className="text-3xl font-bold">Các mục khác</h1>
       <p className="mt-4 text-gray-700">
-        Đây là trang tổng hợp cho chuyên mục "Others".
+        {`Đây là trang tổng hợp cho chuyên mục "Others".`}
       </p>
 
       <ul className="list-disc pl-6 mt-4 space-y-2">
-        <li><a href="/others/guide" className="text-blue-600 hover:underline">Hướng dẫn đặc biệt</a></li>
-        <li><a href="/others/tips" className="text-blue-600 hover:underline">Tips nhỏ hữu ích</a></li>
+        <li>
+          <a href="/others/guide" className="text-blue-600 hover:underline">
+            Hướng dẫn đặc biệt
+          </a>
+        </li>
+        <li>
+          <a href="/others/tips" className="text-blue-600 hover:underline">
+            Tips nhỏ hữu ích
+          </a>
+        </li>
       </ul>
     </div>
   );
